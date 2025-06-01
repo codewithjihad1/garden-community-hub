2afeb0d830a895cf014acb3e8b3915e6db556e17
Md Jihad Hossain <mdjihadhossain793@gmail.com>
2025-06-01 18:36:45 +0600

    some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -129,7 +129,7 @@ const Header = () => {
             </li>
 
             <li className="before:w-0 hover:before:w-full before:bg-green-600 before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] hover:text-green-600 transition-all duration-300 before:left-0 cursor-pointer capitalize">
-              <NavLink to="/share-a-garden-tip">Share a Garden Tip</NavLink>
+              <NavLink to="/share-garden-tip">Share a Garden Tip</NavLink>
             </li>
 
             <li className="before:w-0 hover:before:w-full before:bg-green-600 before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] hover:text-green-600 transition-all duration-300 before:left-0 cursor-pointer capitalize">
